aee551edf5a7ee5e85fbd7c99f5fb66388885028
dikiogres <dikiogres@gmail.com>
2022-09-21 14:35:14 +0700

    feat: jenis lomba dropdown

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const ttl = document.getElementById('ttl');
 const desa = document.getElementById('desa');
 const kecamatan = document.getElementById('kecamatan');
 const telp = document.getElementById('telp');
+const jenisLombaSelect = document.getElementById("jenis-lomba");
+
+const jenisLombaSelection = [
+    { value: 1, text: "Tarik Tambang (4 Perempuan + 1 Laki = 5 orang)" },
+    { value: 2, text: "Lari Estafet (4 Perempuan)" },
+    { value: 3, text: "Gobak Sodor (4 Perempuan)" },
+    { value: 4, text: "Tartil (1 orang perempuan TKA/TPA)" },
+    { value: 5, text: "Ceramah (1 orang perempuan TKA/TPA)" },
+  ];
 
 function letters (name) {
     return /^[a-zA-Z ]+$/.test(name);
@@ -31,5 +40,25 @@ telp.addEventListener ('blur', function (e) {
         event.target.style.background = '';
     }
 });
+
+const displayJenisLomba = (options) => {
+    options.forEach((element) => {
+      const newOption = document.createElement("option");
+      newOption.value = element.value;
+      newOption.text = element.text;
+      jenisLombaSelect.appendChild(newOption);
+    });
+  };
+
+  displayJenisLomba(jenisLombaSelection);
+
+document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+  
+  function onSubmit() {
+    alert("Pendaftaran Berhasil");
+    document.getElementById("form").reset();
+  }
 
 
